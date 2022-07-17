cbe2ef35284e684eb1d21def7cf8215324d8477b
tkshehan <thomas.shehan88@gmail.com>
2022-07-17 19:48:30 -0400

    declare suits

---
+++ index.js
@@ -0,0 +1,15 @@
+// Deckbuilding
+
+
+
+// Declare Suits
+const PINZU = [11, 12, 13, 14, 15, 16, 17, 18, 19]; // Circles 1-9
+const SOUZU = [21, 22, 23, 24, 25, 26, 27, 28, 29]; // Bamboo 1-9
+const MANZU = [31, 32, 33, 34, 35, 36, 37, 38, 39]; // Characters 1-9
+const SANGENPAI = [41, 42, 43]; // Dragons: White, Green, Red
+const KAZEHAI = [51, 52, 53, 54]; // Winds: East, South, West, North
+
+
+// Dealing
+
+// Hand Scoring
