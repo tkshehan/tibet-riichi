48925b0882dbb06ad63b7806d9f13dba4736e18d
tkshehan <thomas.shehan88@gmail.com>
2022-07-17 22:06:56 -0400

    add first yaku

---
+++ yaku.js
@@ -0,0 +1,17 @@
+const YAKU = {
+    'Riichi': {
+        han: 1,
+        menzen: true,
+        en: 'Riichi',
+        jp: 'Riichi'
+    },
+    'Tanyao': {
+        han: 2,
+        menzen: false,
+        jp: 'Tanyao',
+        en: 'All Simples'
+    }
+}
+
+// Yaku worth 1 han less when open
+const KUISIGARI = [YAKU.Tanyao]
