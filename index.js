f9a3d31d552e6bb908c203da3f517699259aaf3b
tkshehan <thomas.shehan88@gmail.com>
2022-10-28 10:21:24 -0400

    formatting

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
 
 // Declare Suits
-
-
-const PIN = [11, 12, 13, 14, 15, 16, 17, 18, 19]; // Circles 1-9, 0 is Red
-const SOU = [21, 22, 23, 24, 25, 26, 27, 28, 29]; // Bamboo 1-9, 0 is Red
-const MAN = [31, 32, 33, 34, 35, 36, 37, 38, 39]; // Characters 1-9, 0 is Red
-const WIND = [41, 42, 43]; // Dragons: White, Green, Red
-const DRAGON = [51, 52, 53, 54]; // Winds: East, South, West, North
-
-const SUITS = {
-  PIN: PIN,
-  SOU: SOU,
-  MAN: MAN,
-  WIND: WIND,
-  DRAGON: DRAGON
-};
+const PIN = Object.freeze([11, 12, 13, 14, 15, 16, 17, 18, 19]); // Circles 1-9
+const SOU = Object.freeze([21, 22, 23, 24, 25, 26, 27, 28, 29]); // Bamboo 1-9
+const MAN = Object.freeze([31, 32, 33, 34, 35, 36, 37, 38, 39]); // Characters 1-9
+const SANGEN = Object.freeze([41, 42, 43]); // Dragons: White, Green, Red
+const KAZE = Object.freeze([51, 52, 53, 54]); // Winds: East, South, West, North
 
 // Deckbuilding
 const WALL = [];
 
+buildWall(PIN);
+console.log('Tiles:', WALL);
+
 function buildWall(...suits) {
-    for (suit of suits) {
-      suit.map(a => WALL.push(a, a, a, a))
-    }
-    console.log('Tiles:', WALL);
+  for (suit of suits) {
+    suit.map(a => WALL.push(a, a, a, a))
+  }
 }
-buildWall(SUITS.PIN);
 
 // Shuffling
 
-
 // Dealing
+class Hand {
+  constructor() {
+    this.tiles = [];
+    this.melds = [];
+  }
+  validate() {
+    // Check for complete hand
+  }
+}
 
 
 // Hand Scoring
@@ -39,5 +38,9 @@ buildWall(SUITS.PIN);
     // We need to check small hands for learning games, but also hands that have tiles called out of it.
     // Each group must be a sequence or a triplet (Kans will be implemented later, as they require unique identifiers)
     // The pair should always be last
+    // only check values lower than 40 for Chi, seperate hand into suits?
     var testHand = [11, 12, 12, 13, 13, 14, 22, 22, 22, 23, 24, 54, 54, 54];
+    var testStep = [[11, 12, 12, 13, 14], [22, 22, 22, 23, 24], [54, 54, 54]]; //Potentially, however this shouldnt be nessicary?
     var expectedGroups = [[11, 12, 13], [12, 13, 14], [22, 23, 24], [54, 54, 54], [22, 22]]
+
+
